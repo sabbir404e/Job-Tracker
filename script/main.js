7aee3ecb66c3faacab0f8092b964f3f4ffeda92e
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-03-02 12:00:34 +0600

    js code uplod

---
+++ script/main.js
@@ -0,0 +1,19 @@
+let currentTab = "all";
+const tabActive = ["btn-primary"];
+const tabInactive = ["bg-transparent", "text-slate-700"];
+
+function switchTab(tab){
+console.log(tab);
+const tabs = ["all", "interview", "rejected"];
+
+for(const t of tabs){
+    const tabName = document.getElementById("tab-"+t);
+    if(t==tab){
+        tabName.classList.remove(...tabInactive);
+        tabName.classList.add(...tabActive);
+    }
+    else{
+        tabName.classList.add(...tabInactive);
+    }
+}
+}
